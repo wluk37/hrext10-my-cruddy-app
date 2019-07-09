e463d2451b4c30a87a71fff0b18bab01b5bb6e43
Wesley Luk <wesleyluk@wesleys-mbp.gateway.sonic.net>
2019-07-09 11:57:39 -0700

    Created Events for Buttons

--- app/app.js
+++ app/app.js
@@ -1,30 +1,36 @@
-/*
 
- ### Basic Reqs
-- [ ] Where to store data? (localstorage)
-- [ ] How to modify data? (update action, delete action)
-
-*/
-
-//localStorage functions
-var createItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
+////////////////////////////
+// localStorage functions //
+////////////////////////////
+var addItem = function(key, value){
+    window.localStorage.setItem(key, value);
 }
 
-var updateItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
+var updateItem = function(key, value){
+    window.localStorage.setItem(key, value);
 }
 
-var deleteItem = function(key) {
-  return window.localStorage.removeItem(key);
+var removeItem = function(key){
+  window.localStorage.removeItem(key);
 }
 
-var clearDatabase = function() {
-  return window.localStorage.clear();
+var clearDatabase = function(){
+  window.localStorage.clear();
 }
 
-var showDatabaseContents = function() {
-  $('tbody').html('');
+
+var keyExists = function(key){
+  return window.localStorage.getItem(key) !== null;
+}
+var getIngredientKey = function(){
+  return $("#ingredient-key").val();
+}
+var getIngredientVal = function(){
+  return $("#ingredient-value").val();
+}
+
+var showDatabaseContents = function(){
+    $('tbody').html('');
 
   for (var i = 0; i < window.localStorage.length; i++) {
     var key = window.localStorage.key(i);
@@ -32,79 +38,69 @@ var showDatabaseContents = function() {
   }
 }
 
-var keyExists = function(key) {
-  return window.localStorage.getItem(key) !== null
-}
-
-var getKeyInput = function() {
-  return $('.key').val();
-}
-
-var getValueInput = function() {
-  return $('.value').val();
-}
-
-var resetInputs = function() {
-  $('.key').val('');
-  $('.value').val('');
-}
-
 $(document).ready(function() {
   showDatabaseContents();
+////////////////////////////
+//     button  events     //
+////////////////////////////
 
-  $('.create').click(function() {
-    if (getKeyInput() !== '' && getValueInput() !== '') {
-      if (keyExists(getKeyInput())) {
-        if(confirm('key already exists in database, do you want to update instead?')) {
-          updateItem(getKeyInput(), getValueInput());
-          showDatabaseContents();
-        }
-      } else {
-        createItem(getKeyInput(), getValueInput());
-        showDatabaseContents();
-        resetInputs();
-      }
-    } else  {
-      alert('key and value must not be blank');
-    }
-  });
-
-  $('.update').click(function() {
-    if (getKeyInput() !== '' && getValueInput() !== '') {
-      if (keyExists(getKeyInput())) {
-        updateItem(getKeyInput(), getValueInput());
-        showDatabaseContents();
-        resetInputs();
-      } else {
-        alert('key does not exist in database');
-      }
+// ADD INGREDIENT //
+$("#add-ingredient").click(function(){
+  if (getIngredientKey() !== 'INGREDIENT' && getIngredientVal() !== 'AMOUNT'){
+    if ((!keyExists(getIngredientKey())) && (!keyExists(getIngredientKey().toLowerCase()))){
+       addItem(getIngredientKey(), getIngredientVal());
+       showDatabaseContents();
     } else {
-      alert('key and value must not be blank');
+      alert("Ingredient already exists!")
     }
-  });
-
-  $('.delete').click(function() {
-     if (getKeyInput() !== '') {
-      if (keyExists(getKeyInput())) {
-        deleteItem(getKeyInput());
-        showDatabaseContents();
-        resetInputs();
-      } else {
-        alert('key does not exist in database');
-      }
-    } else {
-      alert('key must not be blank');
-    }
-  });
-
-  $('.reset').click(function() {
-    resetInputs();
-  })
-
-  $('.clear').click(function() {
-    if (confirm('WARNING: Are you sure you want to clear the database? \n                THIS ACTION CANNOT BE UNDONE')) {
-      clearDatabase();
-      showDatabaseContents();
-    }
-  })
+  } else {
+    alert("Please fill in the blanks")
+  } 
 })
+
+// UPDATE INGREDIENT //
+$("#update-ingredient").click(function(){
+  if (getIngredientKey() !== '' && getIngredientVal() !== ''){
+    if ((keyExists(getIngredientKey().toLowerCase())) || (keyExists(getIngredientKey()))){
+      addItem(getIngredientKey(), getIngredientVal());
+      showDatabaseContents();
+    } else {
+      alert("Ingredient does not exist!")
+    }
+  } else {
+    alert("Please fill in the blanks")
+  }
+})
+
+// REMOVE INGREDIENT //
+$("#remove-ingredient").click(function(){
+  if (getIngredientKey() !== ''){
+    (removeItem(getIngredientKey()) && removeItem(getIngredientKey().toLowerCase()))
+    showDatabaseContents();
+  } else {
+    alert("Please fill in the blanks")
+  }
+})
+
+// REMOVE ALL ITEMS IN DATABASE //
+$("#clear-storage").click(function(){
+  if (confirm("ARE YOU SURE YOU WANT TO ERASE RECIPE?")){
+    clearDatabase();
+    showDatabaseContents();
+  }
+})
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
