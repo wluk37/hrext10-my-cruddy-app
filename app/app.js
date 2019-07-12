7904eea7f31ffba192ba28a573a04129cdd454b3
Wesley Luk <wesleyluk@wesleys-mbp.gateway.sonic.net>
2019-07-11 23:04:17 -0700

    Finished Phase 2!

--- app/app.js
+++ app/app.js
@@ -22,15 +22,15 @@ var updateItem = function(key, value){
 }
 
 var removeItem = function(key){
-  window.localStorage.removeItem(key);
+  delete pantry[key];
+  window.localStorage.setItem("ingredients", JSON.stringify(pantry));
 }
 
-var clearDatabase = function(){
-  window.localStorage.clear();
+var clearIngredients = function(){
+  window.localStorage.removeItem("ingredients");
   pantry = {}
   window.localStorage.setItem("ingredients", JSON.stringify(pantry))
 }
-
 
 var keyExists = function(key){
   var keys = Object.keys(pantry)
@@ -49,21 +49,87 @@ var getRecipeTitle = function(){
   return $("recipe-title").val();
 }
 var showIngredients = function(){
-  $('tbody').html('');
+  $('#ingredient-body').html('');
   var pan = JSON.parse(window.localStorage.getItem("ingredients"));
   for (var key in pan) {
-    $('tbody').append(`<tr><td>${key}</td><td>${pan[key][0]}</td><td>${pan[key][1]}</td></tr>`)
+    $('#ingredient-body').append(`<tr><td>${key}</td><td>${pan[key][0]}</td><td>${pan[key][1]}</td></tr>`)
+  }
+}
+
+// Equipment Cupboard //
+var cupboard = {}
+
+if (window.localStorage.getItem("equipment") !== "{}"){
+  cupboard = JSON.parse(window.localStorage.getItem("equipment"));
+}
+window.localStorage.setItem("equipment", JSON.stringify(cupboard))
+
+var addEquip = function(key, value){
+    cupboard[key] = value
+    window.localStorage.setItem("equipment", JSON.stringify(cupboard));
+}
+
+var updateEquip = function(key, value){
+    cupboard[key] = value
+    window.localStorage.setItem("equipment", JSON.stringify(cupboard));
+}
+
+var removeEquip = function(key){
+  delete cupboard[key];
+  window.localStorage.setItem("equipment", JSON.stringify(cupboard));
+}
+
+var clearEquip = function(){
+  window.localStorage.removeItem("equipment");
+  cupboard = {}
+  window.localStorage.setItem("equipment", JSON.stringify(cupboard))
+}
+
+var equipExists = function(key){
+  var keys = Object.keys(cupboard)
+  return keys.includes(key);
+}
+var getEquipKey = function(){
+  return $("#equipment-key").val();
+}
+var getEquipVal = function(){
+  return $("#equipment-value").val();
+}
+
+var showEquip = function(){
+  $('#equipment-body').html('');
+  var cup = JSON.parse(window.localStorage.getItem("equipment"));
+  for (var key in cup) {
+    $('#equipment-body').append(`<tr><td>${key}</td><td>${cup[key][0]}</td></tr>`)
   }
 }
 
 
 $(document).ready(function() {
+  showEquip();
   showIngredients();
+
+
+
+
+
+
+
 
 /////////////////////////////
 //      button  events     //
 /////////////////////////////
 
+// WIPE ENTIRE RECIPE //
+
+$("#clear-storage").click(function(){
+  if (confirm("CATASTROPHE! CALAMITY! DOOOOOOOM! \n CERTAIN YE ARE, ERASE RECIPE YOU WILL")){
+    clearIngredients();
+    clearEquip();
+    showIngredients();
+    showEquip();
+  }
+})
 // SAVE RECIPE TITLE //
 
 if(localStorage.getItem("recipeTitle")){
@@ -119,10 +185,10 @@ $("#remove-ingredient").click(function(){
   }
 })
 
-// REMOVE ALL ITEMS IN DATABASE //
-$("#clear-storage").click(function(){
-  if (confirm("ARE YOU SURE YOU WANT TO ERASE RECIPE?")){
-    clearDatabase();
+// REMOVE ALL INGREDIENTS IN DATABASE //
+$("#clear-pantry").click(function(){
+  if (confirm("Are you sure you want to clear pantry? Action irreversible.")){
+    clearIngredients();
     showIngredients();
   }
 })
@@ -132,6 +198,51 @@ $("#clear-storage").click(function(){
 
 
 
+// ADD EQUIPMENT //
+$("#add-equipment").click(function(){
+  if (getEquipKey() !== '' && getEquipVal() !== ''){
+    if ((!equipExists(getEquipKey())) && (!equipExists(getEquipKey().toLowerCase()))){
+       addEquip(getEquipKey(), getEquipVal());
+       showEquip();
+    } else {
+      alert("Equipment already exists!")
+    }
+  } else {
+    alert("Please fill in the blanks")
+  } 
+})
+
+// UPDATE EQUIPMENT //
+$("#update-equipment").click(function(){
+  if (getEquipKey() !== '' && getEquipVal() !== ''){
+    if ((equipExists(getEquipKey().toLowerCase())) || (equipExists(getEquipKey()))){
+      updateEquip(getEquipKey(), getEquipVal());
+      showEquip();
+    } else {
+      alert("Equipment does not exist!")
+    }
+  } else {
+    alert("Please fill in the blanks")
+  }
+})
+
+// REMOVE EQUIPMENT //
+$("#remove-equipment").click(function(){
+  if (getEquipKey() !== ''){
+    (removeEquip(getEquipKey()) && removeEquip(getEquipKey().toLowerCase()))
+    showEquip();
+  } else {
+    alert("Please fill in the blanks")
+  }
+})
+
+// REMOVE ALL EQUIPMENT IN DATABASE //
+$("#clear-cupboard").click(function(){
+  if (confirm("Are you sure you want to clear cupboard? Action irreversible.")){
+    clearEquip();
+    showEquip();
+  }
+})
 })
 
 
